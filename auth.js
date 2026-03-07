c8bff20cf3b57259bf42749589b3cd1e62a1b16b
Shimu Akter <shimuakter.pciu@gmail.com>
2026-03-08 00:01:42 +0600

    authentication js added

---
+++ auth.js
@@ -0,0 +1,22 @@
+function login(){
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+if(username === "admin" && password === "admin123"){
+
+localStorage.setItem("login", true);
+
+window.location.href = "index.html";
+
+}
+else{
+
+alert("Wrong username or password");
+
+}
+
+}
